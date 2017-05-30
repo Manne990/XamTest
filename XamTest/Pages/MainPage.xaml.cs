c05e7f7b48240c61962770a6c2d2e7c2ed7d7f0d
Jonas Frid <jonas.frid@bybrick.se>
2017-05-30 08:43:47 +0200

    Fix

--- XamTest/Pages/MainPage.xaml.cs
+++ XamTest/Pages/MainPage.xaml.cs
@@ -14,85 +14,22 @@ namespace XamTest
         {
             InitializeComponent();
 
-            MaskPhoneNumber("(123) 556-7890");
-            MaskPhoneNumber("08-123 45 67");
-            MaskPhoneNumber("021-12 34 56");
-            MaskPhoneNumber("021-123456");
-            MaskPhoneNumber("021123456");
-            MaskPhoneNumber("+46 21 12 34 56");
-            MaskPhoneNumber("+4621123456");
-            MaskPhoneNumber("12345");
-            MaskPhoneNumber("12");
-            MaskPhoneNumber("1");
-            MaskPhoneNumber("");
-
-            //var grid = new Grid
-            //    {
-            //        VerticalOptions = LayoutOptions.FillAndExpand,
-            //        HorizontalOptions = LayoutOptions.FillAndExpand,
-            //        RowDefinitions = 
-            //            {
-            //                new RowDefinition { Height = new GridLength(100, GridUnitType.Absolute) },
-            //                new RowDefinition { Height = new GridLength(1, GridUnitType.Star) }
-            //            },
-            //        ColumnDefinitions = 
-            //            {
-            //                new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) },
-            //            }
-            //        };
-
-            //var container = new Frame
-            //{
-            //    Content = new SubPage1().Content,
-            //    BackgroundColor = Color.Aqua,
-            //    VerticalOptions = LayoutOptions.FillAndExpand,
-            //    HorizontalOptions = LayoutOptions.FillAndExpand
-            //};
-
-            //var button1 = new Button() { Text = "SubPage1" };
-
-            //button1.Clicked += (object sender, EventArgs e) => {
-            //    container.Content = new SubPage1().Content;
-            //};
-
-            //var button2 = new Button() { Text = "SubPage2" };
-
-            //button2.Clicked += (object sender, EventArgs e) => {
-            //    container.Content = new SubPage2().Content;
-            //};
-
-            //grid.Children.Add(new StackLayout {
-            //    Children = { 
-            //        button1,
-            //        button2
-            //    },
-            //    BackgroundColor = Color.Aqua,
-            //    Padding = new Thickness(10),
-            //    VerticalOptions = LayoutOptions.FillAndExpand,
-            //    HorizontalOptions = LayoutOptions.FillAndExpand
-            //}, 0, 0);
-
-            //grid.Children.Add(container, 0, 1);
-
-            //grid.BackgroundColor = Color.Gray;
-            //this.Padding = new Thickness(20);
-            //Content = grid;
+            //RealImage.IsVisible = false;
         }
 
-        public static string MaskPhoneNumber(string phonenumber)
+        protected async override void OnAppearing()
         {
-            if (string.IsNullOrWhiteSpace(phonenumber))
-            {
-                return string.Empty;
-            }
+            base.OnAppearing();
 
-            var length = phonenumber.Length < 5 ? 1 : 4;
-            var index = phonenumber.Length < 6 ? 0 : phonenumber.Length - 6;
-            var masked = phonenumber.Remove(index, Math.Min(length, phonenumber.Length - index)).Insert(index, new string('*', length));
+            Placeholder.Source = "https://static.pexels.com/photos/7976/pexels-photo.jpg";
+            //Placeholder.Source = "testbild.jpg";
 
-            System.Diagnostics.Debug.WriteLine($"{phonenumber} -> {masked}");
+            //await Task.Delay(3000);
 
-            return masked;
+            //Placeholder.IsVisible = false;
+
+            //RealImage.IsVisible = true;
+            //RealImage.Source = "https://static.pexels.com/photos/7976/pexels-photo.jpg";
         }
     }
 }
